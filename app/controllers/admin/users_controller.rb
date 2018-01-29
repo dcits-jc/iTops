10fd18ee04355435913d058853d0ba7e858b533b
liangchaob <liangchaob@163.com>
2018-01-30 05:22:58 +0800

    search add

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,8 +3,10 @@ class Admin::UsersController < ApplicationController
   
 
   def index
-    @users = User.all
-    @sbus = Sbu.all
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
+
+
   end
 
   def new
