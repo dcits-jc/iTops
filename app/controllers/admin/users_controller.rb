c98d05f5df814393f6747d01cb8b61ba186efb02
liangchaob <liangchaob@163.com>
2018-02-06 15:27:00 +0800

    add csv

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,7 +4,7 @@ class Admin::UsersController < ApplicationController
 
   def index
     @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true)
+    @users = @q.result(distinct: true).paginate(:page => params[:page], :per_page => 30)
 
 
   end
